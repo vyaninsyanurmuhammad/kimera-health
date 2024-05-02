ada0c174084223ca7a1d0f2a8cd4278024192ec5
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-05-02 18:49:06 +0700

    refactor: :lipstick: add background color layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-screen bg-background font-sans antialiased bg-dodger-blue-100",
           inter.variable
         )}
       >
