08a79024ae82e53482a6878f22e4201162328c99
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-05-05 21:29:23 +0700

    feat: :lipstick: add partners

--- src/components/home-testimonial-form.tsx
+++ src/components/home-testimonial-form.tsx
@@ -4,12 +4,24 @@ import { Button } from "./ui/button";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
+import PartnerTag from "./partner-tag";
 
 const HomeTestimonialForm = () => {
   return (
     <>
-      <div className="w-full flex flex-col items-center px-8 xl:px-28 gap-12 z-20">
-        <div className="w-fit flex flex-col gap-6 items-center">
+      <div className="w-full flex flex-row justify-center gap-12 xl:px-28 z-20">
+        <div className="hidden xl:flex w-full h-auto flex-col gap-28">
+          <div className="flex justify-end">
+            <PartnerTag src="image 31.png" alt="partner-1-1" isMargin={false} />
+          </div>
+          <div className="flex justify-start">
+            <PartnerTag src="image 30.png" alt="partner-1-2" isMargin={false} />
+          </div>
+          <div className="flex justify-end">
+            <PartnerTag src="image 41.png" alt="partner-1-3" isMargin={false} />
+          </div>
+        </div>
+        <div className="w-fit xl:shrink-0 flex flex-col gap-6 items-center">
           <TagSection color="emerald" text="featured products" />
           <p className="text-4xl/snug xl:text-5xl/snug font-semibold text-white text-start">
             Let&apos;s Start the Conversation
@@ -49,7 +61,7 @@ const HomeTestimonialForm = () => {
               <Label htmlFor="full-name" className="text-white font-normal">
                 Message&nbsp;<span className="text-red-500">*</span>
               </Label>
-              <Textarea 
+              <Textarea
                 className="w-full py-5 px-5 bg-dodger-blue-200/20 text-white border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
                 rows={10}
                 placeholder="Input your message"
@@ -59,6 +71,19 @@ const HomeTestimonialForm = () => {
           <Button className="rounded-full px-16 py-6 bg-dodger-blue-500 hover:bg-dodger-blue-500/90">
             <span>Start a Conversation</span>
           </Button>
+        </div>
+        <div className="hidden xl:flex w-full h-auto flex-col justify-end gap-28">
+          <div className="flex justify-start">
+            <PartnerTag src="image 37.png" alt="partner-2-1" isMargin={false} />
+          </div>
+
+          <div className="flex justify-end">
+            <PartnerTag src="image 36.png" alt="partner-2-2" isMargin={false} />
+          </div>
+
+          <div className="flex justify-start">
+            <PartnerTag src="image 32.png" alt="partner-2-3" isMargin={false} />
+          </div>
         </div>
       </div>
     </>
--- src/components/partner-tag.tsx
+++ src/components/partner-tag.tsx
@@ -4,13 +4,14 @@ import React from "react";
 type PartnerTagProps = {
   src: string;
   alt: string;
+  isMargin?: boolean;
 };
 
-const PartnerTag = ({ src, alt }: PartnerTagProps) => {
+const PartnerTag = ({ src, alt, isMargin = true }: PartnerTagProps) => {
   return (
     <>
-      <div className="h-fit w-full flex justify-center mx-5">
-        <div className="rounded-full h-16 w-fit py-4 px-10 bg-white relative">
+      <div className={`h-fit flex justify-center ${isMargin ? "mx-5 w-full" : "w-fit"}`}>
+        <div className={`rounded-full h-16 w-fit py-4 px-10 bg-white relative`}>
           <Image
             src={`/assets/images/partners/${src}`}
             className="!relative object-contain h-full w-fit"
